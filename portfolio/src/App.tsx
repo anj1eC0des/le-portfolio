ed2c235bcfaacbe55f9a8efeeeaf4b932a4e9e31
anj1eC0des <129589930+anj1eC0des@users.noreply.github.com>
2025-03-19 09:47:45 +0000

    add avatar component and text component and make changes to margin and background

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from './components/ui/button'
+import { Card } from './components/ui/card'
+// import { AvatarImage } from './components/ui/avatar'
+import { Avatar, AvatarImage } from './components/ui/avatar'
+import { AvatarFallback } from '@radix-ui/react-avatar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='dark '>
+      <Card className="rounded-none min-h-screen min-w-screen px-20">
+        <div className='grid grid-cols-3 gap-4 p-2 '>
+        <Avatar className="justify-self-center my-auto h-fit">
+          <AvatarImage></AvatarImage>
+          <AvatarFallback>SD</AvatarFallback>
+        </Avatar>
+        <Card className='col-span-2 h-fit'>
+          <div className="flex justify-start px-2 italic ">
+            Hi! I am Denn :3 Nice to meet you!<br/>
+            I am a backend dev and I like working on exciting problems.<br/>
+          </div>
+        </Card>
+        </div>
+      </Card>
+    </div>
     </>
   )
 }
